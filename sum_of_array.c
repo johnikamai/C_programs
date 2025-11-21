fc5a0a6ab99ff2789aa67d37b17679b9e2512298
johnikamai <johnikamai2006@gmail.com>
2025-11-21 13:10:10 +0530

    Create sum_of_array.c

---
+++ sum_of_array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int a[100], n, i;
+    printf("Enter array size: ");
+    scanf("%d", &n);
+    printf("\n Enter array values:");
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        sum = sum + a[i];
+    }
+
+    printf("Sum of array values = %d", sum);
+    return 0;
+}
